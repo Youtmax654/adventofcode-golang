3bedaa7cd810e051f2f01134326bf7422d6af242
Youtmax654 <pennmaxime@gmail.com>
2024-08-09 17:52:46 +0200

    adding modification of ChatGPT

--- 2015/day03/part2/main.go
+++ 2015/day03/part2/main.go
@@ -19,35 +19,56 @@ func main() {
 
 	for i, direction := range string(directions) {
 		if i % 2 == 0 {
-			p := santaPosition
-			switch direction {
-			case '^':
-				p.y++
-			case 'v':
-				p.y--
-			case '>':
-				p.x++
-			case '<':
-				p.x--
-			}
-			santaPosition = p
-			houses[p] = true
+			// p := santaPosition
+			// switch direction {
+			// case '^':
+			// 	p.y++
+			// case 'v':
+			// 	p.y--
+			// case '>':
+			// 	p.x++
+			// case '<':
+			// 	p.x--
+			// }
+			// santaPosition = p
+			// houses[p] = true
+			// Corrected by ChatGPT
+			santaPosition = move(santaPosition, direction)
+			houses[santaPosition] = true
 		} else {
-			p := robotPosition
-			switch direction {
-			case '^':
-				p.y++
-			case 'v':
-				p.y--
-			case '>':
-				p.x++
-			case '<':
-				p.x--
-			}
-			robotPosition = p
-			houses[p] = true
+			// p := robotPosition
+			// switch direction {
+			// case '^':
+			// 	p.y++
+			// case 'v':
+			// 	p.y--
+			// case '>':
+			// 	p.x++
+			// case '<':
+			// 	p.x--
+			// }
+			// robotPosition = p
+			// houses[p] = true
+			// Corrected by ChatGPT
+			robotPosition = move(robotPosition, direction)
+			houses[robotPosition] = true
 		}
 	}
 
 	fmt.Printf("There is %d houses that received at least one present.\n", len(houses))
+}
+
+// Added by ChatGPT
+func move(pos coordinate, direction rune) coordinate {
+	switch direction {
+	case '^':
+		pos.y++
+	case 'v':
+		pos.y--
+	case '>':
+		pos.x++
+	case '<':
+		pos.x--
+	}
+	return pos
 }
